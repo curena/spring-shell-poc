5b1018194a2c71500add76c6317fd7b1cf6fad90
Cecil Urena <cecil.urena@contrastsecurity.com>
2022-05-27 09:54:05 -0500

    Initial commit

---
+++ src/main/java/org/cecil/springshellpoc/SpringShellPocApplication.java
@@ -0,0 +1,13 @@
+package org.cecil.springshellpoc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringShellPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringShellPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/cecil/springshellpoc/SpringShellPocApplicationTests.java
@@ -0,0 +1,13 @@
+package org.cecil.springshellpoc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringShellPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
